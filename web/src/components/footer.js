3596cae0eb12e251fa0d1aaa6c19f164e4837119
Brandon Schenz <bschenz@gmail.com>
2019-12-04 12:53:53 -0500

    add years of service content

--- web/src/components/footer.js
+++ web/src/components/footer.js
@@ -2,63 +2,104 @@ import { Link } from "gatsby"
 import React from "react"
 
 const Footer = () => (
-  <span>
-    <footer className="footer primary-footer">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-2 col-sm-2"></div>
-        
-          <div className="col-md-4 col-sm-4">
-            <div className="widget clearfix">
-              <h4 className="widget-title">Company</h4>
-              <ul>
-                <li><Link to="/about/">About us</Link></li>
-                <li><Link to="/contact/">Contact</Link></li>
-              </ul>
-            </div>
-          </div>
-        
-          <div className="col-md-4 col-sm-4">
-            <h4 className="widget-title text-center">Products</h4>
+    <span>
+        <footer className="footer primary-footer">
             <div className="container">
-              <div className="row">
-                <div className="col-md-3 col-sm-3">
-                  <div className="widget clearfix">
-                      <ul>
-                        <li><Link to="/rewards/">Rewards</Link></li>
-                        <li><Link to="/peertopeer/">Peer to Peer</Link></li>
-                        <li><Link to="/performanceimprovement/">Performance Improvement</Link></li>
-                        <li><Link to="/bestpractices/">Best Practices</Link></li>
-                      </ul>
-                  </div>
-                </div>
+                <div className="row">
+                    <div className="col-md-2 col-sm-2"></div>
 
-                <div className="col-md-2 col-sm-2">
-                  <div className="widget clearfix">
-                    <ul>
-                      <li><Link to="/safety/">Safety</Link></li>
-                      <li><Link to="/health/">Health</Link></li>
-                      <li><Link to="/contributionbonus/">Contribution Bonus</Link></li>
-                    </ul>
-                  </div>
+                    <div className="col-md-4 col-sm-4">
+                        <div className="widget clearfix">
+                            <h4 className="widget-title">Company</h4>
+                            <ul>
+                                <li>
+                                    <Link to="/about/">About us</Link>
+                                </li>
+                                <li>
+                                    <Link to="/contact/">Contact</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+
+                    <div className="col-md-4 col-sm-4">
+                        <h4 className="widget-title text-center">Products</h4>
+                        <div className="container">
+                            <div className="row">
+                                <div className="col-md-3 col-sm-3">
+                                    <div className="widget clearfix">
+                                        <ul>
+                                            <li>
+                                                <Link to="/rewards/">
+                                                    Rewards
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/peertopeer/">
+                                                    Peer to Peer
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/performanceimprovement/">
+                                                    Performance Improvement
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/bestpractices/">
+                                                    Best Practices
+                                                </Link>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+
+                                <div className="col-md-2 col-sm-2">
+                                    <div className="widget clearfix">
+                                        <ul>
+                                            <li>
+                                                <Link to="/yearsofservice/">
+                                                    Years of Service
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/safety/">
+                                                    Safety
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/health/">
+                                                    Health
+                                                </Link>
+                                            </li>
+                                            <li>
+                                                <Link to="/contributionbonus/">
+                                                    Contribution Bonus
+                                                </Link>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </footer>
-  
-    <footer className="footer secondary-footer">
-      <div className="container">
-        <div className="row">
-          <div className="col-md-6 col-sm-6 col-xs-12">
-            <p>{new Date().getFullYear()} &copy;<Link to="/">PerformanceTrak</Link> All rights reserved.</p>
-          </div>
-        </div>
-      </div>
-    </footer>
-  </span>
+        </footer>
+
+        <footer className="footer secondary-footer">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6 col-sm-6 col-xs-12">
+                        <p>
+                            {new Date().getFullYear()} &copy;
+                            <Link to="/">PerformanceTrak</Link> All rights
+                            reserved.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </span>
 )
 
 export default Footer
--- web/src/pages/yearsofservice.js
+++ web/src/pages/yearsofservice.js
@@ -1,14 +1,36 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const YearsOfService = () => (
-  <Layout>
-    <SEO title="Years Of Service" />
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+    <Layout>
+        <SEO title="Years Of Service" />
+        <section className="section overfree">
+            <div className="container">
+                <div className="section-title text-center">
+                    <h3>Years Of Service</h3>
+                    <hr />
+                    <p className="lead">
+                        Congratulate your employees for their commitment and
+                        dedication to your company. We offers a modern approach
+                        to service awards that maximize your budget while
+                        maintaining a high perceived value.
+                    </p>
+                    <p className="lead">
+                        Through our innovative team of designers and partners,
+                        We offers a variety of awards that give employees the
+                        ability to choose the award that means the most to them.
+                        Whether it be a monogrammed jacket, customized piece of
+                        artwork, or desktop award, you are no longer confined to
+                        a specific catalog. Our approach guarantees a
+                        cost-effective way to celebrate service while offering
+                        something for everyone.
+                    </p>
+                </div>
+            </div>
+        </section>
+    </Layout>
 )
 
 export default YearsOfService

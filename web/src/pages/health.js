bc75a2cd30b88d230cd9ea9f854b25222c4d95ce
Brandon Schenz <bschenz@gmail.com>
2019-12-04 12:50:00 -0500

    add health content

--- web/src/pages/health.js
+++ web/src/pages/health.js
@@ -1,14 +1,32 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Health = () => (
-  <Layout>
-    <SEO title="Health" />
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+    <Layout>
+        <SEO title="Health" />
+        <section className="section overfree">
+            <div className="container">
+                <div className="section-title text-center">
+                    <h3>Health</h3>
+                    <hr />
+                    <p className="lead">
+                        Recognize your employees for their healthy and active
+                        lifestyles. Create custom health and wellness
+                        initiatives to meet your business goals.
+                    </p>
+                    <p className="lead">
+                        Use our platform to design your program structure, track
+                        progress for wellness goals, and display rewards and
+                        recognition for employee success. Wellness programs can
+                        boost employee morale, reduce healthcare-related costs,
+                        and improve the productivity in your workplace.
+                    </p>
+                </div>
+            </div>
+        </section>
+    </Layout>
 )
 
 export default Health

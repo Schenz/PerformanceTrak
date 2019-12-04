85a7f8a2b2b327f82b8a664b8c33b0e806ba162f
Brandon Schenz <bschenz@gmail.com>
2019-12-04 11:37:20 -0500

    create peer-to-peer content

--- web/src/pages/peertopeer.js
+++ web/src/pages/peertopeer.js
@@ -1,14 +1,29 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const PeerToPeer = () => (
-  <Layout>
-    <SEO title="Peer To Peer" />
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+    <Layout>
+        <SEO title="Peer To Peer" />
+        <section className="section overfree">
+            <div className="container">
+                <div className="section-title text-center">
+                    <h3>Peer-To-Peer</h3>
+                    <hr />
+                    <p className="lead">
+                        Recognition at the peer level is one of the best ways to
+                        boost productivity internally. Empowering employees with
+                        the tools to recognize others and be recognized
+                        on-the-spot for a job well done lets them know their
+                        daily efforts aren’t going unnoticed. Research shows
+                        that employees who feel recognized are more apt to
+                        produce better results and remain loyal to the company.
+                    </p>
+                </div>
+            </div>
+        </section>
+    </Layout>
 )
 
 export default PeerToPeer

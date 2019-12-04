efc0bc382ef1e6ebc828a709abc20ecd98432467
Brandon Schenz <bschenz@gmail.com>
2019-12-04 12:46:18 -0500

    add best practices content

--- web/src/pages/bestpractices.js
+++ web/src/pages/bestpractices.js
@@ -1,14 +1,31 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const BestPractices = () => (
-  <Layout>
-    <SEO title="Best Practices" />
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+    <Layout>
+        <SEO title="Best Practices" />
+        <section className="section overfree">
+            <div className="container">
+                <div className="section-title text-center">
+                    <h3>Best Practices</h3>
+                    <hr />
+                    <p className="lead">
+                        Best Practices is a social learning platform to capture
+                        best practices, recognize employees, and distribute
+                        critical tribal knowledge throughout your organization.
+                        Our solution goes beyond classroom learning and
+                        e-learning to directly involve the workforce in content
+                        creation and knowledge sharing. This tool alleviates
+                        communication bottlenecks and allows your employees to
+                        connect and work together across departments, regions,
+                        and generations.
+                    </p>
+                </div>
+            </div>
+        </section>
+    </Layout>
 )
 
 export default BestPractices

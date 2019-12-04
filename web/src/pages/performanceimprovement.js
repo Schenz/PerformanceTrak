f3a54b7fbfd089917c6608387a799cb3f2debe0c
Brandon Schenz <bschenz@gmail.com>
2019-12-04 12:44:04 -0500

    add performance improvement content

--- web/src/pages/performanceimprovement.js
+++ web/src/pages/performanceimprovement.js
@@ -1,14 +1,48 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const PerformanceImprovement = () => (
-  <Layout>
-    <SEO title="Performance Improvement" />
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+    <Layout>
+        <SEO title="Performance Improvement" />
+        <section className="section overfree">
+            <div className="container">
+                <div className="section-title text-center">
+                    <h3>Strategic &amp; Long-Term Incentives</h3>
+                    <hr />
+                    <p className="lead">
+                        Utilize the platform to create data dashboards and
+                        scorecards for individuals and groups throughout an
+                        enterprise or multi-channel organization. Managers can
+                        examine analytics that will show performance trends and
+                        identify top performers or groups, as well as
+                        individuals who require performance interventions. Your
+                        data and content is presented “one-to-one”, delivering
+                        the right amount of information to each individual based
+                        on their business role, goals, and current status of
+                        performance attainment.
+                    </p>
+                </div>
+                <div className="section-title text-center">
+                    <h3>Tactical &amp; Short-Term Incentives</h3>
+                    <hr />
+                    <p className="lead">
+                        While strategic programs serve the long-term
+                        organizational objectives, sometimes short-term tactical
+                        programs are needed to help achieve specific, immediate
+                        business needs. With program Builder, managers can
+                        quickly and easily create and manage short-term
+                        programs, track employee progress and improvement, and
+                        incentivize individuals through rewards and recognition.
+                        These turnkey programs will motivate your sales and
+                        service teams to improve their performance while
+                        contributing to the overall goals of the organization.
+                    </p>
+                </div>
+            </div>
+        </section>
+    </Layout>
 )
 
 export default PerformanceImprovement

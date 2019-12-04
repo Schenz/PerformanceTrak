71fb012e9d89078172019352b3fd30654843813e
Brandon Schenz <bschenz@gmail.com>
2019-12-04 12:48:17 -0500

    add safety content

--- web/src/pages/safety.js
+++ web/src/pages/safety.js
@@ -5,10 +5,32 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const Safety = () => (
-  <Layout>
-    <SEO title="Safety" />
-    <Link to="/">Go back to the homepage</Link>
-  </Layout>
+    <Layout>
+        <SEO title="Safety" />
+        <section className="section overfree">
+            <div className="container">
+                <div className="section-title text-center">
+                    <h3>Safety</h3>
+                    <hr />
+                    <p className="lead">
+                        Safety programs are designed to reward those who create
+                        a safe work environment. By designing your own in-house
+                        safety initiative, you can recognize those who are
+                        contributing to meeting the safety goals of the company
+                        while increasing productivity and profits.
+                    </p>
+                    <p className="lead">
+                        Use our platform to design your program structure, track
+                        progress toward your goals, and display rewards and
+                        recognition for employees. Our flexible platform allows
+                        you to respond to changes in company business objectives
+                        while empowering employees with the tools they need to
+                        be in charge of their own success.
+                    </p>
+                </div>
+            </div>
+        </section>
+    </Layout>
 )
 
 export default Safety

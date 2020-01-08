c32ee1a8c8b23103f867cdfb31764de08179c594
Brandon Schenz <bschenz@gmail.com>
2020-01-08 12:42:22 -0500

    clean up logging

--- PerformanceTrakFunctions/Functions/AddUser.cs
+++ PerformanceTrakFunctions/Functions/AddUser.cs
@@ -20,7 +20,6 @@ namespace PerformanceTrakFunctions.Functions
         {
             try
             {
-                Console.WriteLine("Deserialize Entity");
                 var entity = JsonConvert.DeserializeObject<UserEntity>(await new StreamReader(req.Body).ReadToEndAsync());
 
                 if (entity == null)
@@ -28,7 +27,6 @@ namespace PerformanceTrakFunctions.Functions
                     return new BadRequestObjectResult("User not passed");
                 }
                 
-                Console.WriteLine("get tableName based on Environment");
                 var environment = (Environments)int.Parse(Environment.GetEnvironmentVariable("ENVIRONMENT"));
                 var environmentString = $"{environment.ToString()}";
                 var tableName = $"Users{(environment != Environments.PROD ? environmentString : string.Empty)}";
@@ -36,23 +34,19 @@ namespace PerformanceTrakFunctions.Functions
                     .Parse(Environment.GetEnvironmentVariable("TABLESTORECONNECTIONSTRING"))
                     .CreateCloudTableClient()
                     .GetTableReference(tableName);
-                Console.WriteLine($"tableName: {tableName}");
-                Console.WriteLine($"Create Table if it does not Exist");
+                
                 await table.CreateIfNotExistsAsync();
 
                 entity.PartitionKey = entity.FamilyName.Substring(0, 1);
                 entity.RowKey = entity.Id.ToString();
 
-                Console.WriteLine($"Insert Entity");
                 var result = await table.ExecuteAsync(TableOperation.Insert(entity));
 
-                Console.WriteLine($"return result");
                 return new CreatedResult("", entity);
             }
             catch (Exception ex)
             {
-                Console.WriteLine(ex);
-                log.LogCritical(ex, "Error Adding User");
+                log.LogCritical(ex, $"Error Adding User: {ex.Message}");
                 return new BadRequestObjectResult("Error While Adding User");
             }
         }
